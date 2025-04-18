94dbbafab40195f5b6a7582f3a48111a0ae17172
MohamedRaafatEliwa <mohamed.raafat.eliwa@gmail.com>
2025-04-18 16:58:23 +0200

    Models improved

--- TechXpress.Data/Models/Order.cs
+++ TechXpress.Data/Models/Order.cs
@@ -18,6 +18,6 @@ namespace TechXpress.Data.Models
 
         public decimal TotalAmount { get; set; }
 
-        public required ICollection<OrderDetails> OrderDetails { get; set; }
+        public required ICollection<OrderDetail> OrderDetails { get; set; }
     }
 }
--- TechXpress.Services/obj/Debug/net8.0/TechXpress.Services.AssemblyInfo.cs
+++ TechXpress.Services/obj/Debug/net8.0/TechXpress.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechXpress.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e2564ce555f7c364800cedcca93578d29edad56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb4555de230cbaded901def31aea3003d8472754")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechXpress.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechXpress.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
