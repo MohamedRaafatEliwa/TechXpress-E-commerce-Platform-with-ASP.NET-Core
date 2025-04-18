a87c8190275c2ad2d661949ca621d4648e1541c1
MohamedRaafatEliwa <mohamed.raafat.eliwa@gmail.com>
2025-04-18 15:44:23 +0200

    OrderDetails has been added

---
+++ TechXpress.Data/Models/OrderDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechXpress.Data.Models
+{
+    public class OrderDetail:BaseEntity
+    {
+        [ForeignKey(nameof(Order))]
+        public int OrderId { get; set; }
+
+        [ForeignKey(nameof(Product))]
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; } 
+
+        public virtual Order? Order { get; set; }
+        public virtual Product? Product { get; set; }
+    }
+}
--- TechXpress.Data/obj/Debug/net8.0/TechXpress.Data.AssemblyInfo.cs
+++ TechXpress.Data/obj/Debug/net8.0/TechXpress.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechXpress.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3817d9e7730a3ffe849b4a94317f2f87fcdc97a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b6b86e03f828e4510c7b0d2efbfb9530d8f8a40")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechXpress.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechXpress.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
