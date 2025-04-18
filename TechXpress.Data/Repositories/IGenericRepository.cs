387c93474f7d5e5b7492b7060760ba60c4118788
MohamedRaafatEliwa <mohamed.raafat.eliwa@gmail.com>
2025-04-18 19:30:21 +0200

    IGenericRepository has been Created

---
+++ TechXpress.Data/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechXpress.Data.Models;
+
+namespace TechXpress.Data.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync(bool trackChanges = true);
+        Task<T?> GetByIdAsync(int id , bool trackChanges = true);
+        Task<T?> GetByUuidAsync(string Uuid, bool trackChanges = true);
+        Task AddAsync(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+        Task DeleteAsync(int id);
+    }
+}
