968433b1e65e77e22d5630c6564bc92b9f8cbbeb
MohamedRaafatEliwa <mohamed.raafat.eliwa@gmail.com>
2025-05-02 00:47:25 +0300

    Brand Model has been created

---
+++ TechXpress.Data/Models/Brand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechXpress.Data.Models
+{
+    public class Brand : BaseEntity
+    {
+        public required string Name { get; set; }
+        public string? LogoUrl { get; set; }
+
+
+        // Navigation property
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+
+    }
+}
